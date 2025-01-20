ed76341afc666219ffdcfaf6880abc391e4cc487
Dilshodbek Donaboyev <d.donaboyev@uic.group>
2025-01-20 17:40:11 +0500

    add: register

--- apps/bot/handlers/commands.py
+++ apps/bot/handlers/commands.py
@@ -1,14 +1,35 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.utils.i18n import gettext as _
 from aiogram.utils.i18n import lazy_gettext as __
 
 from apps.bot.keyboards.reply import reply_main_menu
+from apps.bot.utils.states import RegisterStatesGroup
 
+from django.contrib.auth import get_user_model
 
 router = Router()
+User = get_user_model()
+
 
 @router.message(CommandStart())
 async def start_command(message: types.Message, state: FSMContext):
-    await message.answer(_("Botga xush kelibsiz!"), reply_markup=reply_main_menu())
+    try:
+        user = await User.objects.aget(id = message.from_user.id)    
+
+        if user:
+            await message.answer(_("Botga xush kelibsiz!"), reply_markup=reply_main_menu())
+
+        else:
+            await message.answer(_("Keling siz bilan yaqindan tanishib olamiz 😊"))
+            await message.answer(_("Ismingiz: "))
+            await state.set_state(RegisterStatesGroup.full_name)
+        
+    except Exception as err:
+        await message.answer(_("Xatolik yuz berdi 😢"))
+
+
+@router.message(F.text, RegisterStatesGroup.full_name)
+def register_full_name(messages: types.Message, state: FSMContext):
+    ...
--- apps/bot/utils/states.py
+++ apps/bot/utils/states.py
@@ -5,3 +5,7 @@ class SettingsMenuStatesGruop(StatesGroup):
     menu = State()
     language = State()
 
+class RegisterStatesGroup(StatesGroup):
+    full_name = State()
+    phone = State()
+    language = State()
